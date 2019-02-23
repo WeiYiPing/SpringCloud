699d272828618f7e227d8fe3b08c3b419f325d45
weiyiping <2385951234@qq.com>
2019-02-23 20:18:28 +0800

    首次提交

---
+++ SpringCloud/src/main/java/com/study/SpringCloud/App.java
@@ -0,0 +1,13 @@
+package com.study.SpringCloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
